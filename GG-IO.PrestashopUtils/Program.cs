8232e9944f6c29a11cae70d62832c60023011b74
Ludovic Godrant <ludovic.godrant@gg-io.com>
2020-05-05 16:03:07 +0200

    add code

---
+++ GG-IO.PrestashopUtils/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GG_IO.PrestashopUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
